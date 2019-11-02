e4ed7ad76d887ede3334b8ef20ccc8d01ae49405
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-02 16:15:28 +0600

    Random movie with customers
    Random movie with its customer

---
+++ Vidly App/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random( )
+        {
+            var movie = new Movie() { Name = "Shrek"};
+
+            var customers = new List<Customer>
+            {
+                new Customer() {Name = "Mehedi"},
+                new Customer() {Name = "Rony"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {   
+                Movie = movie,
+                Customers =  customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
