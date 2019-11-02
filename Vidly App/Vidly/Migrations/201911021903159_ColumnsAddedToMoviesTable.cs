ad782bd23f5a7475d273ba140ddd86fe96b08e1e
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-03 01:17:03 +0600

    Movies List with additional Information
    Full movies table and show in page from database

---
+++ Vidly App/Vidly/Migrations/201911021903159_ColumnsAddedToMoviesTable.cs
@@ -0,0 +1,22 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ColumnsAddedToMoviesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "DateAdded", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false, precision: 7, storeType: "datetime2"));
+            AddColumn("dbo.Movies", "NumberInStock", c => c.Byte(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "NumberInStock");
+            DropColumn("dbo.Movies", "ReleaseDate");
+            DropColumn("dbo.Movies", "DateAdded");
+        }
+    }
+}
--- Vidly App/Vidly/Models/Movie.cs
+++ Vidly App/Vidly/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,22 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        public byte GenreId { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime DateAdded { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime ReleaseDate { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+
     }
 }
