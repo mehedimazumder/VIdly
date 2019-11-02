5c7aecbb5b2577466650e5a41b881c31d5b9b076
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-03 01:56:53 +0600

    Add form to Customers
    Added a sample form without button toLink  ./customers/new

--- Vidly App/Vidly/Controllers/CustomersController.cs
+++ Vidly App/Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -37,6 +38,18 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+
+            var viewModel = new NewCustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
         }
 
     }
--- Vidly App/Vidly/Models/Customer.cs
+++ Vidly App/Vidly/Models/Customer.cs
@@ -17,11 +17,14 @@ namespace Vidly.Models
         public string Name { get; set; }
 
         [Column(TypeName = "datetime2")]
+        [Display(Name = "Date of Birth")]
+
         public DateTime? Birthdate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
 
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
             
     }
