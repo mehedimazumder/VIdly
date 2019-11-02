eb5c4d5c5614afe4e60e7fe7205cfb6d8ca6662b
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-02 17:10:44 +0600

    Customer and Movie details
    Add index and details of customer and movie

--- Vidly App/Vidly/Controllers/MoviesController.cs
+++ Vidly App/Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,8 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            return View();
+            var movies = GetMovies();
+            return View(movies);
         }
 
         public ActionResult Random( )
@@ -33,6 +34,25 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-e"}
+            };
         }
     }
 }
--- Vidly App/Vidly/Models/Customer.cs
+++ Vidly App/Vidly/Models/Customer.cs
@@ -8,6 +8,7 @@ namespace Vidly.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
