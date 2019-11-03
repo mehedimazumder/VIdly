a965b47440285359b3667e218fa5a483a1b67e6b
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-03 23:01:04 +0600

    Movies Api created

--- Vidly App/Vidly/Controllers/Api/CustomersController.cs
+++ Vidly App/Vidly/Controllers/Api/CustomersController.cs
@@ -21,9 +21,9 @@ namespace Vidly.Controllers.Api
         }
 
         //GET /api/customers
-        public IEnumerable<CustomerDto> GetCustomers()
+        public IHttpActionResult GetCustomers()
         {
-            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+            return Ok(_context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>));
         }
 
         //GET /api/customers/{id}
@@ -53,34 +53,38 @@ namespace Vidly.Controllers.Api
             return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
         }
 
-        //PUT /api/customers/{id}
+        // PUT /api/customers/1
         [HttpPut]
-        public void UpdateCustomer(int id, CustomerDto customerDto)
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                return BadRequest();
 
             var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-            if(customerInDb == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (customerInDb == null)
+                return NotFound();
 
             Mapper.Map(customerDto, customerInDb);
 
             _context.SaveChanges();
+
+            return Ok();
         }
 
-        //DELETE /api/customers/{id}
+        // DELETE /api/customers/1
         [HttpDelete]
-        public void DeleteCustomer(int id)
+        public IHttpActionResult DeleteCustomer(int id)
         {
             var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customerInDb == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+                return NotFound();
 
             _context.Customers.Remove(customerInDb);
             _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
--- Vidly App/Vidly/Dtos/MovieDto.cs
+++ Vidly App/Vidly/Dtos/MovieDto.cs
@@ -1,36 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace Vidly.Models
+namespace Vidly.Dtos
 {
-    public class Movie
+    public class MovieDto
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
 
-        
-        public Genre Genre { get; set; }
         [Required]
-        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
-        [Column(TypeName = "datetime2")]
         public DateTime DateAdded { get; set; }
 
-        [Column(TypeName = "datetime2")]
-        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
-        [Display(Name = "Number in stocks")]
         [Range(1, Int32.MaxValue)]
         public byte NumberInStock { get; set; }
-
-
     }
 }
