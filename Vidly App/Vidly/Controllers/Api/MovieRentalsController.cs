48559c8aa6fcfb09a1ea8418323372a58526ef9e
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-05 16:31:51 +0600

    Defensive edge cases approach
    It is good for building public api where explicit error are written widely

--- Vidly App/Vidly/Controllers/Api/MovieRentalsController.cs
+++ Vidly App/Vidly/Controllers/Api/MovieRentalsController.cs
@@ -26,12 +26,24 @@ namespace Vidly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateMovieRentals(MovieRentalDto movieRental)
         {
-            var customer = _context.Customers.Single(c => c.Id == movieRental.CustomerId);
+            if (movieRental.MovieIds.Count == 0)
+                return BadRequest("Movie IDs invalid");
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == movieRental.CustomerId);
+
+            if (customer == null)
+                return BadRequest("customer not found");
 
             var movies = _context.Movies.Where(m => movieRental.MovieIds.Contains(m.Id)).ToList();
 
+            if (movies.Count != movieRental.MovieIds.Count)
+                return BadRequest("One or more Movie Ids are invalid");
+
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Not available");
+
                 movie.NumberAvailable--;
                 var rental = new Rental
                 {
